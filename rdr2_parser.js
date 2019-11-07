49e0cda9b7e4540aa320a298c4cc128f96a4f5ff
Jeroen Baert <3607063+Forceflow@users.noreply.github.com>
2019-11-07 17:08:13 +0100

    Added optional features, percentages for advanced settings

--- rdr2_parser.js
+++ rdr2_parser.js
@@ -7,60 +7,59 @@ var $xml;
 var valid_xml = true;
 
 // define value -> setting translation
-var MAP_ANISO = {}; MAP_ANISO["0"] = "Off";MAP_ANISO["1"] = "2x";MAP_ANISO["2"] = "4x";MAP_ANISO["3"] = "8x";MAP_ANISO["4"] = "16x";
-var REFL_MSAA = {}; REFL_MSAA["0"] = "Off";REFL_MSAA["1"] = "2x";REFL_MSAA["2"] = "4x";REFL_MSAA["3"] = "8x";
+var MAP_ANISO = {}; MAP_ANISO["0"] = "Off"; MAP_ANISO["1"] = "2x"; MAP_ANISO["2"] = "4x"; MAP_ANISO["3"] = "8x"; MAP_ANISO["4"] = "16x";
+var REFL_MSAA = {}; REFL_MSAA["0"] = "Off"; REFL_MSAA["1"] = "2x"; REFL_MSAA["2"] = "4x"; REFL_MSAA["3"] = "8x";
 
-$( document ).ready(function() {
-    watcharea();
-});
+$(document).ready(function () {watcharea();});
 
-function getValuefromXML(a){
+function range_to_percent(val,min,max){
+	var percentage = (val-min)/(max-min) * 100;
+	return Math.floor(percentage) + "%";
+}
+
+function getValuefromXML(a) {
 	return $xml.find(a).attr("value");
 }
 
-function getTextfromXML(a){
+function getTextfromXML(a) {
 	return $xml.find(a).text();
 }
 
-function int_to_yesno(value){
-	if(value == 0){return "Yes";}else{return "No";}
+function int_to_yesno(value) {
+	if (value == 0) { return "Yes"; } else { return "No"; }
 }
 
-function bool_to_onoff(value){
-	if(value = "true"){return "On";}
-	if(value = "false"){return "Off";}
+function bool_to_onoff(value) {
+	if (value = "true") { return "On"; }
+	if (value = "false") { return "Off"; }
 	return "undefined";
 }
 
-function watcharea(){
-	$('textarea#inifile').on('change',function(){
-		parse();
-	});
-	$('textarea#inifile').keyup(function(){
-		parse();
-	});
+function watcharea() {
+	$('textarea#inifile').on('change', function () {parse();});
+	$('textarea#inifile').keyup(function () {parse();});
 }
 
 function parseXML() {
 	valid_xml = true;
 	var inifile = $('textarea#inifile').val();
-	if(inifile == ""){
+	if (inifile == "") {
 		valid_xml = false;
 		return;
 	} try {
-		var xmlDoc = $.parseXML( inifile );
-	} catch(err) {
+		var xmlDoc = $.parseXML(inifile);
+	} catch (err) {
 		valid_xml = false;
 		return;
 	}
-	$xml = $( xmlDoc );
+	$xml = $(xmlDoc);
 }
 
-function writeLine(line){
+function writeLine(line) {
 	$("#parsed").val($("#parsed").val() + line + "\n");
 }
 
-function writeSettings(){
+function writeSettings() {
 	// Screen and API settings
 	var width = getValuefromXML("screenWidth");
 	var height = getValuefromXML("screenHeight");
@@ -71,10 +70,6 @@ function writeSettings(){
 	var pauseonfocusloss = int_to_yesno(getValuefromXML("pauseOnFocusLoss"));
 	var videocard = getTextfromXML("videoCardDescription");
 	var api = getTextfromXML("API").split("_")[1];
-	writeLine(videocard + ", " + api);
-	writeLine(width + " x " + height + ", " + refreshrate + "hz, Windowed: " + windowed + ", Vsync: " + vsync + ", Triple: " +triplebuffer);
-	writeLine("");
-
 	// MAIN SETTINGS
 	var textures = getTextfromXML("textureQuality").split("_")[1];
 	var anisotropic = MAP_ANISO[getValuefromXML("anisotropicFiltering")];
@@ -92,7 +87,36 @@ function writeSettings(){
 	var fxaa = int_to_yesno(getValuefromXML("fxaaEnabled"));
 	var taa = getTextfromXML("taa").split("_")[1];
 	var msaa = getValuefromXML("msaa");
+	// ADVANCED SETTINGS
+	var locked = getValuefromXML("locked");
+	var near_volum = getTextfromXML("volumetricsLightingQuality").split("_")[1];
+	var far_volum = getTextfromXML("volumetricsRaymarchQuality").split("_")[1];
+	var volum_quality = getTextfromXML("volumetricsLightingQuality").split("_")[1];
+	var particle_lighting = getTextfromXML("particleLightingQuality").split("_")[1];
+	var unlock_volum_res = bool_to_onoff(getValuefromXML("volumetricsRaymarchResolutionUnclamped"));
+	var soft_shadows = getTextfromXML("shadowSoftShadows").split("_")[1];
+	var grass_shadows = getTextfromXML("shadowGrassShadows").split("_")[1];
+	var long_shadows = bool_to_onoff(getValuefromXML("shadowLongShadows"));
+	var full_res_ssao = bool_to_onoff(getValuefromXML("ssaoFullScreenEnabled"));
+	var water_refraction = getTextfromXML("waterRefractionQuality").split("_")[1];
+	var water_reflection = getTextfromXML("waterReflectionQuality").split("_")[1];
+	var water_physics_quality = range_to_percent(getValuefromXML("waterSimulationQuality"),0,4);
+	var res_scale = getTextfromXML("scalingMode").split("_")[1]; // TODO: parse this better
+	var taa_sharp = range_to_percent(getValuefromXML("sharpenIntensity"),0,1);
+	var motion_blur = bool_to_onoff(getValuefromXML("motionBlur"));
+	var refl_msaa = MAP_ANISO[getValuefromXML("reflectionMSAA")];
+	var geometry_lod = range_to_percent(getValuefromXML("lodScale"), 0.75, 1);
+	var grass_lod = range_to_percent(getValuefromXML("grassLod"),0,3);
+	var tree = getTextfromXML("treeQuality").split("_")[1];
+	var pom = getTextfromXML("POMQuality").split("_")[1];
+	var decal = getTextfromXML("decalQuality").split("_")[1];
+	var fur = getTextfromXML("furDisplayQuality").split("_")[1];
 
+	// WRITE EVERYTHING (NORMAL MODE)
+	if ($("#quote").is(":checked")) { writeLine("[QUOTE]"); }
+	writeLine(videocard + ", " + api);
+	writeLine(width + " x " + height + ", " + refreshrate + "hz, Windowed: " + windowed + ", Vsync: " + vsync + ", Triple: " + triplebuffer);
+	writeLine("");
 	writeLine("Textures: " + textures);
 	writeLine("Anisotropic: " + anisotropic);
 	writeLine("Lighting: " + lighting);
@@ -107,36 +131,8 @@ function writeSettings(){
 	writeLine("Particles: " + particle);
 	writeLine("Tessellation: " + tessellation);
 	writeLine("FXAA: " + fxaa + ", TAA: " + taa + ", MSAA: " + msaa);
+	if (locked == "true" && $("#hide_advanced").is(":checked")) { if ($("#quote").is(":checked")) { writeLine("[/QUOTE]"); } return; } // we stop here: advanced settings have been locked.
 	writeLine("");
-
-	// ADVANCED SETTINGS
-	var locked = getValuefromXML("locked");
-	writeLine("Advanced Settings Locked: " + locked)
-	// if(locked == "true"){return;} // we stop here: advanced settings have been locked.
-
-	var near_volum = getTextfromXML("volumetricsLightingQuality").split("_")[1]; // Todo: check correctness
-	var far_volum = getTextfromXML("volumetricsRaymarchQuality").split("_")[1]; // Todo: check correctness
-	var volum_quality = getTextfromXML("volumetricsLightingQuality").split("_")[1];
-	var particle_lighting = getTextfromXML("particleLightingQuality").split("_")[1];
-	var unlock_volum_res = bool_to_onoff(getValuefromXML("volumetricsRaymarchResolutionUnclamped"));
-	var soft_shadows = getTextfromXML("shadowSoftShadows").split("_")[1];
-	var grass_shadows = getTextfromXML("shadowGrassShadows").split("_")[1];
-	var long_shadows = bool_to_onoff(getValuefromXML("shadowLongShadows"));
-	var full_res_ssao = bool_to_onoff(getValuefromXML("ssaoFullScreenEnabled"));
-	var water_refraction = getTextfromXML("waterRefractionQuality").split("_")[1];
-	var water_reflection = getTextfromXML("waterReflectionQuality").split("_")[1];
-	var water_physics_quality = getValuefromXML("waterSimulationQuality");
-	var res_scale = getTextfromXML("scalingMode").split("_")[1]; // TODO: parse this better
-	var taa_sharp = getValuefromXML("sharpenIntensity");
-	var motion_blur = bool_to_onoff(getValuefromXML("motionBlur"));
-	var refl_msaa = MAP_ANISO[getValuefromXML("reflectionMSAA")];
-	var geometry_lod = "TODO"; // I think it's lodScale, but that doesn't scale linearly?
-	var grass_lod = getValuefromXML("grassLod");
-	var tree = getTextfromXML("treeQuality").split("_")[1];
-	var pom = getTextfromXML("POMQuality").split("_")[1];
-	var decal = getTextfromXML("decalQuality").split("_")[1];
-	var fur = getTextfromXML("furDisplayQuality").split("_")[1];
-
 	writeLine("Near Volumetrics Quality: " + near_volum);
 	writeLine("Far Volumetric Quality: " + far_volum);
 	writeLine("Volumetric Lighting Quality: " + volum_quality);
@@ -148,34 +144,37 @@ function writeSettings(){
 	writeLine("Full resolution SSAO: " + full_res_ssao);
 	writeLine("Water Refraction Quality: " + water_refraction)
 	writeLine("Water Reflection Quality: " + water_reflection)
-	writeLine("Water Physics Quality: " + water_physics_quality + " (range 0-4)");
+	writeLine("Water Physics Quality: " + water_physics_quality);
 	writeLine("Resolution scale: " + res_scale);
-	writeLine("TAA Sharpen Intensity: " + taa_sharp + " (range 0-1)");
+	writeLine("TAA Sharpen Intensity: " + taa_sharp);
 	writeLine("Motion Blur: " + motion_blur);
 	writeLine("Reflection MSAA: " + refl_msaa);
 	writeLine("Geometry Level of Detail: " + geometry_lod);
-	writeLine("Grass Level of Detail: " + grass_lod + " (range 0-1)");
+	writeLine("Grass Level of Detail: " + grass_lod);
 	writeLine("Tree Quality: " + tree);
 	writeLine("Parallax Occlusion Mapping Quality: " + pom);
 	writeLine("Decal Quality: " + decal);
 	writeLine("Fur Quality: " + fur);
+
+	writeLine(" ");
+	writeLine("Generated with Forceflow's RDR2 settings parser");
+	if ($("#quote").is(":checked")) { writeLine("[/QUOTE]"); }
 }
 
-function parse(){
+function parse() {
 	$("#parsed").val('');
 	parseXML();
-	if(!valid_xml){
+	if (!valid_xml) {
 		writeLine("No XML or invalid XML pasted. Make sure you paste the full contents of your settings.xml file in the area on the left!");
 		return;
 	}
 	writeSettings();
-	writeLine(" ");
-	writeLine("Generated with Forceflow's RDR2 settings parser");
+
 }
 
-function loadExample(){
+function loadExample() {
 	var client = new XMLHttpRequest();
 	client.open('GET', 'system.xml');
-	client.onreadystatechange = function() {$('textarea#inifile').val(client.responseText); parse();}
+	client.onreadystatechange = function () { $('textarea#inifile').val(client.responseText); parse(); }
 	client.send();
 }
